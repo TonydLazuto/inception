49df65a0c1d3649ee768aa97d3e9c0c23e3fd869
aderose <aderose@vm.here>
2022-05-21 15:30:12 -0400

    details

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -9,9 +9,8 @@ done
 wp-cli config create --allow-root --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$ADMIN_USERNAME --dbpass=$ADMIN_PASSWORD --locale='en_DB'
 #install wordpress
 wp-cli core install --allow-root --url=$DOMAIN_NAME --title=$DB_TITLE --admin_user=$ADMIN_USERNAME --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
-wp-cli user create $DB_USER $DB_USER@4242.fr --role=author --user_pass=$DB_PASSWORD --allow-root
+wp-cli user create $DB_USER $DB_USER@4242.fr --role=administrator --user_pass=$DB_PASSWORD --allow-root
 wp-cli user create $DB_USER2 $DB_USER2@4242.fr --role=editor --user_pass=$DB_PASSWORD2 --allow-root
-# wp theme install twentynineteen --allow-root
-# wp theme activate twentynineteen --allow-root
+wp-cli theme install https://downloads.wordpress.org/theme/colibri-wp.1.0.87.zip --activate --allow-root
 
 exec "$@"
